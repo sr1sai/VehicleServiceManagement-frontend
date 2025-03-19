900bbe468ef29e1103ab5c2e051c521955894f62
Sri Sai Divyakola <srisai.divyakola@quixy.com>
2025-03-19 16:44:02 +0530

    Add manager access guard; implement route guard to restrict access based on user roles and navigate to unauthorized page if access is denied

---
+++ src/app/Gaurds/manager-access.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivateFn, Router } from '@angular/router';
+
+export const managerAccessGuard: CanActivateFn = (route, state) => {
+  const router = new Router();
+  const storedUser = localStorage.getItem('user');
+  if (storedUser) {
+    const user = JSON.parse(storedUser);
+    if (user.role === 2 || user.role === 3) { // Employee, Manager, or Admin role
+      return true;
+    }
+  }
+  router.navigate(['un-authorized']);
+  return false;
+};
